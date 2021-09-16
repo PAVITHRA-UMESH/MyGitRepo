438b0441d3857e9330d4aeb1dc403566ae0f482f
PAVITHRA-UMESH <pavithrau2000@gmail.com>
2021-09-16 20:24:25 +0530

    First commit is made.

---
+++ GitHubTest/src/com/neosoft/MyClass.java
@@ -0,0 +1,14 @@
+package com.neosoft;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		String role="SE";
+		if(role=="SE") {
+		System.out.println("SOFTWARE ENGINEER");
+		}
+		else {
+			System.out.println("NOT A SOFTWARE ENGINEER");
+		}
+	}
+}
